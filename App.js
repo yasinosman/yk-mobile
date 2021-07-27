294d9d911ff0eea6956d908919f8ef77f5ecdf70
Yasin Osman <yasinosman10@gmail.com>
2021-07-27 18:37:10 +0300

    CardView komponenti
    - CardView komponenti oluşturuldu
    - CardView komponentine varsayılan stiller eklendi

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, Text } from "react-native";
 
 //screens
-import Login from './screens/Login';
+import Login from "./screens/Login";
+import CardView from "./components/CardView";
+import { BLUE, GRAY } from "./common/colors";
 
 export default function App() {
-  return (
-    <Login />
-  );
+	return <CardView marginTop={50} marginLeft={50}></CardView>;
 }
-
---
+++ screens/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Login = () => {
+	return <Text>Hello World!!é</Text>;
+};
+
+export default Login;
