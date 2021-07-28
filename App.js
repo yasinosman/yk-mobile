ab495b2c0782b7388643f60c49d3cde5e350093f
Yasin Osman <yasinosman10@gmail.com>
2021-07-28 13:45:09 +0300

    add react native elements to the project

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, Text } from "react-native";
-
-//screens
-import Login from "./screens/Login";
-import CardView from "./components/CardView";
-import { BLUE, GRAY } from "./common/colors";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "react-native-elements";
 
 export default function App() {
-	return <CardView marginTop={50} marginLeft={50}></CardView>;
+	return (
+		<SafeAreaProvider>
+			<ThemeProvider></ThemeProvider>
+		</SafeAreaProvider>
+	);
 }
